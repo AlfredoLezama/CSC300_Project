441f566298d16b499028c7c492fd63267e7501e2
AlfredoLezama <110791811+AlfredoLezama@users.noreply.github.com>
2022-08-28 23:39:12 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -20,7 +20,7 @@ public:
 		price = 0.00;
 	}
 	StandardDrinkOrder(int sel) {
-		drink = stdDrinks[sel];
+		drink = stdDrinks[sel-1];
 		price = sel * 1.00;
 	}
 	double getPrice() {
@@ -28,6 +28,9 @@ public:
 	}
 	void setPrice(double newPrice) {
 		price = newPrice;
+	}
+	string getDrinkName() {
+		return drink;
 	}
 private:
 	string drink;
@@ -81,15 +84,30 @@ public:
 	void addDrink(StandardDrinkOrder d) {
 		drinkArray[numDrinks] = d;
 		total += d.getPrice();
+		numDrinks++;
 	}
 	double getTotal() {
 		return total;
 	}
+	void displayWholeOrder() {
+		cout << "Order\n----------------------------\n";
+		for (int i = 0; i < numDrinks; i++) {
+			cout << i + 1 << "." << setw(9) << drinkArray[i].getDrinkName() << "\tPrice: $" << drinkArray[i].getPrice() << endl;
+		}
+	}
+	void saveReceipt() {
+		fstream receipts;
+		receipts.open("C:\\Users\\jakzi\\Documents\\Receipts.txt", ios::app);
+		receipts << "\nNumber of Drinks: " << numDrinks << "\t Total: $" << getTotal();
+		receipts.close();
+	}
 	int numDrinks;
-private:
 	StandardDrinkOrder drinkArray[10];
+private:
+	
 	double total;
 };
+
 class Cashier {
 public:
 	Cashier(string name) {
@@ -120,50 +138,39 @@ public:
 		incNumOrders();
 	}	
 private:
-	int numberOfOrders;
+	int numberOfOrders =0;
 	DrinkOrder queue[10];
 };
 class Barista {
 public:
 	string name;
 };
-/*class POSGUI {//I'm not sure how to implement this the way I want it....thinking emoji
+class Payment {
 public:
-	void displayTotal(DrinkOrder d1) {
-
+	Payment() {
+		counter = 0;
+		ccReturn = "";
 	}
-	void displayMenu() {
-
+	void creditCheck(DrinkOrder d1, OrderQueue q1) {
+		cout << "Credit Card payment is ";
+		fstream cc;
+		cc.open("C:\\Users\\jakzi\\Documents\\CreditCardProcessor.txt", ios::in);
+		for (int i = 0; i <= counter % 4; i++) {
+			cc >> ccReturn;
+		}
+		cc.close();
+		cout << ccReturn;
+		counter++;
+		if (ccReturn == "Declined.") {
+			cout << "\nYour Card was declined by our processor." << endl;
+		}
+		else {
+			cout << "\nYour card was processed successfully" << endl;
+			q1.addOrder(d1);
+			d1.saveReceipt();
+		}
 	}
-	void displayInit() {
-
-	}
-private:
-	string name;
-};*/
-void displaySplashScreen() {
-	for (int i = 0; i < 5; i++) {
-		cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << endl;
-	}
-	cout << setw(9) << " " << "Welcome to Cafe au Lait" << endl;
-	for (int i = 0; i < 5; i++) {
-		cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << endl;
-	}
-}
-void displayMenu() {
-	cout << "\n\n\n\nMake a Selection for a drink" << endl;
-	cout << "1. Latte\t2. Espresso\t 3. Americano\t4. Mocha Frappe\n";
-
-}
-void orderSelect() {//maybe into a fucntion in the GUI"POS Class
-
-}
-void paymentScreen(DrinkOrder d1 , OrderQueue q1) {
-	cout << "The total for the order is $" << d1.getTotal() << endl;
-	cout << "Did you want to pay with (1)Cash Or (2)Credit? Enter Selection: " << endl;
-	int sel;
-	cin >> sel;
-	if (sel == 1) {
+	void payCash(DrinkOrder d1, OrderQueue q1) {
 		double cashGiven;
 		cout << "Enter Cash Amount: $";
 		cin >> cashGiven;
@@ -173,34 +180,67 @@ void paymentScreen(DrinkOrder d1 , OrderQueue q1) {
 		else {
 			cout << "\nYour change will be $" << cashGiven - d1.getTotal() << endl;
 			q1.addOrder(d1);
+			d1.saveReceipt();
 		}
 	}
-	if (sel == 2) {
-		cout << "Amount being charged to your card is $" << d1.getTotal() << endl;
-		cout << "Please wait for Credit Card processor..." << endl; //okay here I will have a file that says success, failure, sucess for cc payments
-		//if (ccpayment == "success") {
-		//	cout << "hooray" << endl;
-		//}
-		//if (ccpayment != "success") {
-		//	cout << "Card Declined" << endl;
-		//}
-	}
+	void paymentScreen(DrinkOrder d1, OrderQueue q1) {
+		d1.displayWholeOrder();
+		cout << "The total for the order is $" << d1.getTotal() << endl;
+		cout << "Did you want to pay with (1)Cash Or (2)Credit? Enter Selection: " << endl;
+		int sel;
+		cin >> sel;
+		if (sel == 1) {
+			payCash(d1, q1);			
+		}
+		if (sel == 2) {
+			creditCheck(d1, q1);
+		}
 
-}
+	}
+	int counter = 0;
+	string ccReturn;
+};
+class POSGUI {
+public:
+	void displayMenu(DrinkOrder d1) {
+		cout << "\nMake a Selection for a drink" << endl;
+		cout << "1. Espresso\t2. Americano\t 3. Latte\t4. Frappe\n";
+	}
+	void displaySplashScreen(DrinkOrder d1) {
+		/*StandardDrinkOrder temp[10];
+		for (int i = 0; i < 10; i++) {
+			d1.drinkArray[i] = temp[i];
+		}*/
+		for (int i = 0; i < 5; i++) {
+			cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << endl;
+		}
+		cout << setw(9) << " " << "Welcome to Cafe au Lait" << endl;
+		for (int i = 0; i < 5; i++) {
+			cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << endl;
+		}
+	}
+private:
+	string name;
+};
+
 int main()
 {
-	displaySplashScreen();
-	cout << "Enter any character to Start order ";
-	char blank;
-	cin >> blank;
 	DrinkOrder d1;
 	OrderQueue q1;
-	displayMenu();
+	Payment p1;
+	POSGUI POS1;
+
+	char blank;
+	cin >> blank;
+
+	POS1.displaySplashScreen(d1);
+	cout << "Enter any character to Start order ";	
 
 	char orderSel = 'y';
 	for (int i = 0; i < 3; i++) {
 		cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << endl;
 	}
+	POS1.displayMenu(d1);
 	while (d1.numDrinks < 10 && orderSel != 'n'){
 		int drinkSel; //3
 		cin >> drinkSel;
@@ -208,12 +248,9 @@ int main()
 		d1.addDrink(beverage);
 		cout << "Can I get you any more drinks? (y/n)";
 		cin >> orderSel;
-		displayMenu();
+		POS1.displayMenu(d1);
 	}
 	for (int i = 0; i < 3; i++) {
 		cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << endl;
 	}
-	paymentScreen(d1,q1);
-	//cout << "Total: $" << d1.getTotal();
-
-}
+	p1.paymentScreen(d1,q1);
